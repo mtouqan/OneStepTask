11150df06e7236238dcea4b4c92890ab8ee535a5
Munia Touqan <mtouqan@infinitetiers.com>
2020-05-07 11:00:40 +0300

    Creating empty project.

---
+++ OneStepTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneStepTask
+//
+//  Created by Munia Touqan  on 5/7/20.
+//  Copyright © 2020 Munia Touqan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
